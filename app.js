eaef7efd1f771a830b0d58ab4baf1f3f9af8e69d
Damian Wujec <wujecdamian12@gmail.com>
2026-07-03 22:43:31 +0200

    refactor: make routes start with "api/"

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ import postsRouter from "./Routes/postsRouter.js";
 import newUserRouter from "./Routes/newUserRouter.js";
 import logInRouter from "./Routes/logInUserRouter.js";
 
-app.use("/", postsRouter);
-app.use("/register", newUserRouter);
-app.use("/login", logInRouter);
+app.use("api/", postsRouter);
+app.use("api/register", newUserRouter);
+app.use("api/login", logInRouter);
 
 app.listen(3000, () => {
   console.log("App listens on port http://localhost:3000/");
