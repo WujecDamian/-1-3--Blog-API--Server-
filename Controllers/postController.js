a9e0ef4321c8ba47b45a0bf37e367cb956b11cfc
Damian Wujec <wujecdamian12@gmail.com>
2026-07-03 22:37:12 +0200

    feat: add routes and logic for Comments, Posts API (CRUD)

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,8 +1,63 @@
 import { prisma } from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const getAllPosts = async (req, res) => {
   const posts = await prisma.post.findMany();
   res.json(posts);
 };
 
-export { getAllPosts };
+const getPostById = async (req, res) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(req.params.postId),
+    },
+  });
+};
+
+const createComment = async (req, res) => {
+  jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+    if (err) {
+      res.status(403).json({ message: "Invalid token" });
+    } else {
+      const comment = req.body.commentContent;
+      const newComment = await prisma.comment.create({
+        data: {
+          authorId: authData.user.id, //get user id
+          content: comment,
+          postId: Number(req.params.postId),
+        },
+      });
+      res.json({
+        message: "Comment added...",
+        comment: newComment,
+        authData,
+      });
+    }
+  });
+};
+
+const createPost = async (req, res) => {
+  jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+    if (err) {
+      res.status(403).json({ message: "Invalid token" });
+    } else {
+      const content = req.body.content;
+      const title = req.body.title;
+      const newPost = await prisma.post.create({
+        data: {
+          authorId: authData.user.id,
+          content,
+          title,
+        },
+      });
+      res.json({
+        message: "Post added...",
+        post: newPost,
+        authData,
+      });
+    }
+  });
+};
+
+export { getAllPosts, getPostById, createComment, createPost };
--- Routes/postsRouter.js
+++ Routes/postsRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
+import passport from "passport";
 
 const router = express.Router();
 
 import * as postController from "../Controllers/postController.js";
+import verifyToken from "../middleware/verifyToken.js";
 // define the home page route
 router.get("/", postController.getAllPosts);
-router.get("/about", (req, res) => {
-  res.json("About birds");
-});
+router.get("/post/:postId", postController.getPostById);
+router.post("/post/", verifyToken, postController.createPost);
+router.post("/post/:postId/comment", verifyToken, postController.createComment);
 
 export default router;
