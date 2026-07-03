b79c29301f71294cb09c45a3c889fb91390a9940
Damian Wujec <wujecdamian12@gmail.com>
2026-07-03 22:37:49 +0200

    feat: set up routing for user registration and login

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 const app = express();
+app.use(express.json());
 
+//Routing
 import postsRouter from "./Routes/postsRouter.js";
+import newUserRouter from "./Routes/newUserRouter.js";
+import logInRouter from "./Routes/logInUserRouter.js";
 
 app.use("/", postsRouter);
+app.use("/register", newUserRouter);
+app.use("/login", logInRouter);
 
 app.listen(3000, () => {
   console.log("App listens on port http://localhost:3000/");
