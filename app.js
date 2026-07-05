623d2920f073bee855e5e9552270c059ccffbaa8
Damian Wujec <wujecdamian12@gmail.com>
2026-07-05 20:02:58 +0200

    fix: update api routes ( add "/" at start of route)

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ import postsRouter from "./Routes/postsRouter.js";
 import newUserRouter from "./Routes/newUserRouter.js";
 import logInRouter from "./Routes/logInUserRouter.js";
 
-app.use("api/", postsRouter);
-app.use("api/register", newUserRouter);
-app.use("api/login", logInRouter);
+app.use("/api/", postsRouter);
+app.use("/api/register", newUserRouter);
+app.use("/api/login", logInRouter);
 
 app.listen(3000, () => {
   console.log("App listens on port http://localhost:3000/");
