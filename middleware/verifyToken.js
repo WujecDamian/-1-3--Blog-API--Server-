29d78ac909ed4cbd91c2b17ceec69ddea174e62d
Damian Wujec <wujecdamian12@gmail.com>
2026-07-10 22:24:04 +0200

    fix: remove console logs from verifyToken middleware

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,9 +1,7 @@
 function verifyToken(req, res, next) {
   const bearerHeader = req.headers["authorization"];
-  console.log(typeof bearerHeader);
   if (typeof bearerHeader !== "undefined") {
     const bearer = bearerHeader.split(" ");
-    console.log(bearer);
     const bearerToken = bearer[1];
     req.token = bearerToken;
     next();
